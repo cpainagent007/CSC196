77bc51a79afed2231901f9ffa8c3146f75bb9de1
cpainagent007 <cpainagent007@gmail.com>
2025-07-01 10:16:31 -0600

    Removed Renderer Content from Game

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include "Core/Math.h"
-#include "../../Renderer/Renderer.h"
 
 int main(){
 	std::cout << "Hello, World!\n";
 	std::cout << Cpain::pi << std::endl;
-	Cpain::printMessage();
 }
