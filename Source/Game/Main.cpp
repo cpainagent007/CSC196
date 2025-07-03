e77869ad042a4847a0c3a8291c04ea6a3a0df2cd
cpainagent007 <cpainagent007@gmail.com>
2025-07-03 10:01:50 -0600

    Renderer Assignment Completed

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -36,7 +36,25 @@ int main(int argc, char* argv[]) {
         SDL_RenderClear(renderer); // Clear the renderer
 
         SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Set render draw color to green
-        SDL_RenderFillRect(renderer, &greenSquare); // Render the rectangle
+        //SDL_RenderFillRect(renderer, &greenSquare); // Render the rectangle
+
+        for (int i = 0; i < 10; i++) {
+			int r = rand() % 256;
+			int g = rand() % 256;
+			int b = rand() % 256;
+			int a = rand() % 256;
+			SDL_SetRenderDrawColor(renderer, r, g, b, a); // Set random color
+			int startx = rand() % 1280;
+			int starty = rand() % 1024;
+			int endx = rand() % 1280;
+			int endy = rand() % 1024;
+			SDL_RenderLine(renderer, startx, starty, endx, endy); // Render random lines
+        }
+        for (int i = 0; i < 20; i++) {
+			int x = rand() % 1280;
+			int y = rand() % 1024;
+			SDL_RenderPoint(renderer, x, y); // Render random points
+        }
 
         SDL_RenderPresent(renderer); // Render the screen
     }
